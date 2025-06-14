a819b50bf0f7f209f0915dc9cdf364c764665719
Nwoye Emmanuel Emeka <emmanuelnwoye12@gmail.com>
2025-06-14 12:33:00 +0100

    Updated google map

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
         <div className="location-section">
           <div className="map-container">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.078364991944!2d3.3537214742360217!3d6.63719019335727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b93ec633837e3%3A0x6198d33109711b0e!2s9%20Adeyemo%20Akapo%20St%2C%20Omole%20Phase%201%2C%20Ikeja%20101233%2C%20Lagos!5e0!3m2!1sen!2sng!4v1747468252227!5m2!1sen!2sng"
+              src="https://www.google.com/maps?q=Shariff+Plaza,+24+Aminu+Kano+Crescent,+Wuse+2,+Abuja,+Nigeria+904101&output=embed"
               width="100%"
               height="350"
               style={{ border: 0 }}
@@ -21,7 +21,7 @@ const Contact = () => {
           </div>
 
           <a
-            href="https://www.google.com/maps/dir/?api=1&destination=Rs_Arena,+9+Adeyemo+Akapo+Street,+Omole+Phase+1"
+            href="https://www.google.com/maps?q=Shariff+Plaza,+24+Aminu+Kano+Crescent,+Wuse+2,+Abuja,+Nigeria+904101"
             target="_blank"
             rel="noopener noreferrer"
             className="get-directions"
